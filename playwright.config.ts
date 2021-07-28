eb76f9d948857c05790257b22173feb8712dca6b
Yevhen Laichenkov <elaichenkov@gmail.com>
2021-07-28 17:18:45 +0300

    update(config): add screenshot on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,9 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  use: {
+    screenshot: 'only-on-failure'
+  },
   reporter: [
     ['line'], 
     ['experimental-allure-playwright']
